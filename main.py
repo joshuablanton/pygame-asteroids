5381f2b48fd8763d65f7585cf42790612c646996
Joshua Blanton <josh@alphanumerik.com>
2025-04-24 15:08:01 -0700

    Added game clock and limited FPS to 60

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -22,6 +25,8 @@ def main():
                 )
 
         pygame.display.flip()
+
+        dt = (clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
