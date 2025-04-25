29e38ee3cabff813ef7e58e69fee8d82a39fe0eb
Joshua Blanton <josh@alphanumerik.com>
2025-04-24 17:24:11 -0700

    Added player.py and created player in game loop.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -15,6 +16,8 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,6 +26,8 @@ def main():
         screen.fill(
                 (0,0,0),
                 )
+        
+        player.draw(screen)
 
         pygame.display.flip()
 
