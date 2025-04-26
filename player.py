933b00c7708ca73f6d56464463928d7548f68785
Joshua Blanton <josh@alphanumerik.com>
2025-04-25 22:21:55 -0700

    added forward/reverse movement

--- player.py
+++ player.py
@@ -20,13 +20,21 @@ class Player(CircleShape):
         # screen object, color, list of points, line width
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
